b968f3f4d6927df62915cd44ef038078e99396f1
Matt Pooley <Matt.Pooley@autodesk.com>
2018-03-09 06:58:14 +0000

    Updated Ubuntu script

--- Routecheck.sh
+++ Routecheck.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 node /home/ubuntu/RouteCheck/mapsExploration/RouteCheckConv.js >> /home/ubuntu/RouteCheck/mapsExploration/RouteInfoConv.log
 node /home/ubuntu/RouteCheck/mapsExploration/RouteCheckODOT.js >> /home/ubuntu/RouteCheck/mapsExploration/RouteInfoODOT.log
 
